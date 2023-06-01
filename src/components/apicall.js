656c5dc5e0248095fdbb65ca3afe3456a9930c21
unknown <gabrielrozas1996@gmail.com>
2023-06-01 12:40:07 -0800

    Fix linters

--- src/components/apicall.js
+++ src/components/apicall.js
@@ -1,12 +1,14 @@
 const category = 'anger';
 
 const getdata = async () => {
-  const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=' + category, {
+  const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
     headers: {
       'X-Api-Key': 'Fw08AILtO+Qt2bR7wENuqQ==K7dKDmbDUGlMhbEh',
-      'Content-Type': 'application/json'
-    }
-  })
+      'Content-Type': 'application/json',
+    },
+  });
   const data = await response.json();
   return data;
-}
+};
+
+export default getdata;
