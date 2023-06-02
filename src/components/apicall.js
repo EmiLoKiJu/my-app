700c1d98e4619bc4c556fa57db210fb9311c3e76
unknown <gabrielrozas1996@gmail.com>
2023-06-01 16:46:47 -0800

    Add API fetch

--- src/components/apicall.js
+++ src/components/apicall.js
@@ -1,14 +1,33 @@
-const category = 'anger';
+import { useState, useEffect } from 'react';
+import './apicall.css';
 
-const getdata = async () => {
-  const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-    headers: {
-      'X-Api-Key': 'Fw08AILtO+Qt2bR7wENuqQ==K7dKDmbDUGlMhbEh',
-      'Content-Type': 'application/json',
-    },
-  });
-  const data = await response.json();
-  return data;
+const ApiNinja = () => {
+  const category = 'anger';
+  const [data, setData] = useState({ quote: 'LOADING', author: '' });
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+        headers: {
+          'X-Api-Key': 'Fw08AILtO+Qt2bR7wENuqQ==K7dKDmbDUGlMhbEh',
+          'Content-Type': 'application/json',
+        },
+      });
+      const newData = await response.json();
+      setData(newData[0]);
+    };
+    fetchData();
+    return (
+      setData({ quote: 'LOADING', author: '' })
+    );
+  }, []);
+  return (
+    <div>
+      {data.quote}
+      <br />
+      <br />
+      {data.author}
+    </div>
+  );
 };
 
-export default getdata;
+export default ApiNinja;
