2d27ef448eb110ef9c1b8a90b416142c3874505b
unknown <gabrielrozas1996@gmail.com>
2023-06-02 17:27:37 -0800

    Add error state to the API

--- src/components/apicall.js
+++ src/components/apicall.js
@@ -4,22 +4,39 @@ import './apicall.css';
 const ApiNinja = () => {
   const category = 'anger';
   const [data, setData] = useState({ quote: 'LOADING', author: '' });
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-        headers: {
-          'X-Api-Key': 'Fw08AILtO+Qt2bR7wENuqQ==K7dKDmbDUGlMhbEh',
-          'Content-Type': 'application/json',
-        },
-      });
-      const newData = await response.json();
-      setData(newData[0]);
+      try {
+        const response = await fetch(
+          `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+          {
+            headers: {
+              'X-Api-Key': 'Fw08AILtO+Qt2bR7wENuqQ==K7dKDmbDUGlMhbEh',
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error('Error fetching data from the API');
+        }
+
+        const newData = await response.json();
+        setData(newData[0]);
+      } catch (error) {
+        setError(true);
+      }
     };
+
     fetchData();
-    return (
-      setData({ quote: 'LOADING', author: '' })
-    );
   }, []);
+
+  if (error) {
+    return <div>Error 400.</div>;
+  }
+
   return (
     <div>
       {data.quote}
